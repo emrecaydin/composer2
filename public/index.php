6e9c43b4867f2e4202094ddc6b87d1c14c61a29b
emrecaydin <emrecaydin00@gmail.com>
2020-12-01 17:26:19 +0100

    second commit

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+use HelloWorld\SayHello;
+echo SayHello::world();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'emre/composer2',
+  ),
+  'versions' => 
+  array (
+    'ehime/hello-world' => 
+    array (
+      'pretty_version' => '1.0.5',
+      'version' => '1.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b1c8cdd2c11272d8c5deec7816e51fa5374217c1',
+    ),
+    'emre/composer2' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
